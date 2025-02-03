0ff8827ebc81a7339f689e7f58528e4c357abcc6
aelhe <aelheniedy8@gmail.com>
2025-02-03 00:32:43 -0800

    F move almost complete, needs tweakig for top and bottom in sides other than blue

--- rubix.py
+++ rubix.py
@@ -43,48 +43,94 @@ def setCube():
 def printSide(list):
     print(list[0] + " " + list[1] + " " + list[2])
     print(list[3] + " " + list[4] + " " + list[5])
-    print(list[6] + " " + list[7] + " " + list[8])
+    print(list[6] + " " + list[7] + " " + list[8] + "\n")
 
-def nextSide(color):
-    if(color == blue):
-        return orange
-    elif (color == orange):
-        return aqua
-    elif (color == aqua):
-        return black
-    elif (color == black):
-        return blue
-    elif (color == white):
-        return yellow
+def nextSide(color, thisTop):
+    if(thisTop == white):
+        if(color == blue):
+            return orange
+        elif (color == orange):
+            return aqua
+        elif (color == aqua):
+            return black
+        elif (color == black):
+            return blue
     else:
-        return white
+        return prevSide(color, white)
+
+def prevSide(color, thisTop):
+    if(thisTop == white):
+        if(color == blue):
+            return black
+        elif (color == black):
+            return aqua
+        elif (color == aqua):
+            return orange
+        elif (color == orange):
+            return blue
+    else:
+        return nextSide(color, white)
 
 #Following functions are all Rubix cube moves, a png is provided to provide a visual
 
 #Clockwise turn of bottom side
-def D():
-    #Rotates yellow side
-    yellow[:] = [yellow[6], yellow[3], yellow[0], 
-             yellow[7], yellow[4], yellow[1], 
-             yellow[8], yellow[5], yellow[2]]
+def D(bottom):
+    #Rotates bottom side
+    bottom[:] = [bottom[6], bottom[3], bottom[0], 
+             bottom[7], bottom[4], bottom[1], 
+             bottom[8], bottom[5], bottom[2]]
     
-    #Makes a copy of the blue side
-    tempBlue = blue[:]
+    if(bottom == yellow):
+        #Makes a copy of the front side
+        tempFront = front[:]
 
-    #Rotates the bottom 3 pieces of each side
-    blue[6:9] = black[6:9]
-    black[6:9] = aqua[6:9]
-    aqua[6:9] = orange[6:9]
-    orange[6:9] = tempBlue[6:9]
+        #Rotates the bottom 3 pieces of each side
+        temp = prevSide(front,white)
+        front[6:9] = temp[6:9]
+        temp2 = prevSide(temp,white) 
+        temp[6:9] = temp2[6:9]
+        temp = prevSide(temp2,white) 
+        temp2[6:9] = temp[6:9]
+        temp[6:9] = tempFront[6:9]
+    else: #If the bottom side is the white side
+        #Makes a copy of the front side
+        tempFront = front[:]
+
+        #Rotates the bottom 3 pieces of each side
+        temp = prevSide(front,yellow)
+        front[0:3] = temp[0:3]
+        temp2 = prevSide(temp,yellow) 
+        temp[0:3] = temp2[0:3]
+        temp = prevSide(temp2,yellow) 
+        temp2[0:3] = temp[0:3]
+        temp[0:3] = tempFront[0:3]
+
+#Clockwise turn of front face
+def F(front):
+    #Rotates front side
+    front[:] = [front[6], front[3], front[0], 
+             front[7], front[4], front[1], 
+             front[8], front[5], front[2]]
+    
+    # Makes a copy of the left side
+    left = prevSide(front,white)
+    right = nextSide(front,white)
+    tempLeft = left[:]
+
+    # Rotates the sides of the front
+    left[2:9:3] = yellow[0:3]
+    yellow[0:3] = right[0:7:3][::-1]
+    right[0:7:3] = white[6:9]
+    white[6:9] = tempLeft[8:1:-3]
 
 
 def whiteCross():
-
+    F(orange)
+    #F(front)
     pass
 
-printSide(black)
-D()
-print("")
-printSide(black)
+printSide(aqua)
+whiteCross()
+printSide(aqua)
 
 
